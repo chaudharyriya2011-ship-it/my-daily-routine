0eef8e0c2738a8de0507136925867bdf742b51d3
chaudharyriya2011-ship-it <chaudharyriya2011@gmail.com>
2026-09-12 19:33:32 +0530

    initial birthday commit

---
+++ raj.py
@@ -0,0 +1,10 @@
+rahul = "rahul - 1/2/2013"
+sandeep = "sandeep - 5/2/2014"
+darsh = "darsh - 7/9/2014"
+rudra = "rudra - 18/6/2013"
+subodh = "subodh - 7/9/2014"
+print(rahul)
+print(sandeep)
+print(darsh)
+print(rudra)
+print(subodh)
